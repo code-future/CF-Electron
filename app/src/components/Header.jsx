92dd43977738847ac37fc4c070dfac67fc52593f
Zachary Lee <w.zachary.lee@gmail.com>
2017-07-08 16:22:09 -0500

    updated header

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Header } from 'semantic-ui-react'
 export default class AppHeader extends Component {
   render() {
     return(
-      <Header as='h2' dividing>Header</Header>
+      <Header as='h2' dividing>CodeFuture</Header>
       )
   }
 }
